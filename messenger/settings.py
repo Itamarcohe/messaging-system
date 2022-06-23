cc9b720b8e7be30d5d52fe2e0a8f90d532776ae7
Itamar Cohen <Itamar330@gmail.com>
2022-06-23 12:25:48 +0300

    Pushing to server

--- messenger/settings.py
+++ messenger/settings.py
@@ -7,9 +7,8 @@ import os
 load_dotenv()
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-# SECRET_KEY = os.getenv('SECRET_KEY_ABRA')
+SECRET_KEY = os.getenv['SECRET_KEY']
 
-SECRET_KEY = 'c+q(dnc%_gq+joqy6)e-^r(6mwctr3v+htuosdu3^wk)3jh)o4'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
@@ -40,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'messenger.urls'
@@ -114,12 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
-)
 
 
 REST_FRAMEWORK = {
@@ -134,3 +129,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
 }
+
+import dj_database_url
+prod_db=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
