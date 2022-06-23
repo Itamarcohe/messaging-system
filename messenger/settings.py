e67aed3e9e619fc12ad460567d8c1b9473504877
Itamar Cohen <Itamar330@gmail.com>
2022-06-23 11:31:49 +0300

    secret key env

--- messenger/settings.py
+++ messenger/settings.py
@@ -5,20 +5,14 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY_ABRA')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['abra-messaging-system.herokuapp.com']
+ALLOWED_HOSTS = ['abra-messaging-system.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
