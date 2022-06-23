889ef116dbe0230c40da177f70495194592c79c4
Itamar Cohen <96188048+Itamarcohe@users.noreply.github.com>
2022-06-23 17:09:39 +0300

    Update settings.py

--- messenger/settings.py
+++ messenger/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'c+q(dnc%_gq+joqy6)e-^r(6mwctr3v+htuosdu3^wk)3jh)o4'
 SECRET_KEY = os.environ.get('SECRET_KEY_ABRA')
 
 # SECURITY WARNING: don't run with debug turned on in production!
